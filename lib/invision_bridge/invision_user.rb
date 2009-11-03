6b446d5fb965fb7728f52840b9e2ff4600d6b9a3
rspeicher <rspeicher@gmail.com>
2009-11-03 01:16:39 -0500

    Move models into the InvisionBridge module.

--- lib/invision_bridge/invision_user.rb
+++ lib/invision_bridge/invision_user.rb
@@ -1,31 +1,33 @@
-class InvisionUser < ActiveRecord::Base
-  include ActiveRecord::Acts::InvisionBridge
+module InvisionBridge
+  class InvisionUser < ActiveRecord::Base
+    include ActiveRecord::Acts::InvisionBridge
 
-  establish_bridge('members')
+    establish_bridge('members')
 
-  # Authlogic -----------------------------------------------------------------
-  attr_accessible :login, :password, :password_confirmation
-  acts_as_authentic do |c|
-    c.crypto_provider          = Authlogic::CryptoProviders::InvisionPowerBoard
-    c.login_field              = :name
-    c.crypted_password_field   = :converge_password
-    c.password_salt_field      = :converge_salt
-    # c.validate_fields          = false
-    c.validate_password_field  = false
-  end
+    # Authlogic -----------------------------------------------------------------
+    attr_accessible :login, :password, :password_confirmation
+    acts_as_authentic do |c|
+      c.crypto_provider          = Authlogic::CryptoProviders::InvisionPowerBoard
+      c.login_field              = :name
+      c.crypted_password_field   = :converge_password
+      c.password_salt_field      = :converge_salt
+      # c.validate_fields          = false
+      c.validate_password_field  = false
+    end
   
-  def converge_password
-    self.converge.converge_pass_hash
-  end
-  def converge_salt
-    self.converge.converge_pass_salt
-  end
+    def converge_password
+      self.converge.converge_pass_hash
+    end
+    def converge_salt
+      self.converge.converge_pass_salt
+    end
   
-  # Override some AR methods; we don't want to mess with Invision's integrity
-  def destroy; end
-  def delete; end
-  def self.destroy_all; end
-  def self.delete_all; end
+    # Override some AR methods; we don't want to mess with Invision's integrity
+    def destroy; end
+    def delete; end
+    def self.destroy_all; end
+    def self.delete_all; end
   
-  has_one :converge, :class_name => "InvisionUserConverge", :foreign_key => "converge_id"
+    has_one :converge, :class_name => "InvisionUserConverge", :foreign_key => "converge_id"
+  end
 end
--- lib/invision_bridge/invision_user_converge.rb
+++ lib/invision_bridge/invision_user_converge.rb
@@ -1,6 +1,8 @@
-class InvisionUserConverge < ActiveRecord::Base
-  include ActiveRecord::Acts::InvisionBridge
+module InvisionBridge
+  class InvisionUserConverge < ActiveRecord::Base
+    include ActiveRecord::Acts::InvisionBridge
 
-  establish_bridge('members_converge')
-  belongs_to :user, :class_name => "InvisionUser", :foreign_key => "user_id"
+    establish_bridge('members_converge')
+    belongs_to :user, :class_name => "InvisionUser", :foreign_key => "user_id"
+  end
 end
