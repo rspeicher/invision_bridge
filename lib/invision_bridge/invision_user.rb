9150d15a4b0df315a51a934f370c6105166fb82f
rspeicher <rspeicher@gmail.com>
2009-11-03 00:42:13 -0500

    Remove JuggLF-specific stuff from InvisionUser.

--- lib/invision_bridge/invision_user.rb
+++ lib/invision_bridge/invision_user.rb
@@ -2,10 +2,6 @@ class InvisionUser < ActiveRecord::Base
   include ActiveRecord::Acts::InvisionBridge
 
   establish_bridge('members')
-  
-  ADMIN_GROUP     = 4
-  MEMBER_GROUP    = 8
-  APPLICANT_GROUP = 9
 
   # Authlogic -----------------------------------------------------------------
   attr_accessible :login, :password, :password_confirmation
@@ -25,20 +21,11 @@ class InvisionUser < ActiveRecord::Base
     self.converge.converge_pass_salt
   end
   
-  def is_admin?
-    self.mgroup == ADMIN_GROUP
-  end
-  
-  # ---------------------------------------------------------------------------
   # Override some AR methods; we don't want to mess with Invision's integrity
   def destroy; end
   def delete; end
   def self.destroy_all; end
   def self.delete_all; end
   
-  # Relationships -------------------------------------------------------------
   has_one :converge, :class_name => "InvisionUserConverge", :foreign_key => "converge_id"
-  has_one :member, :foreign_key => "user_id"
-  
-  named_scope :juggernaut, :order => 'name', :conditions => {:mgroup => [ADMIN_GROUP, MEMBER_GROUP, APPLICANT_GROUP]}
 end
