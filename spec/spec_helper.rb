793c09336eb9727e8294e9c0afdf0515c62eebaf
rspeicher <rspeicher@gmail.com>
2009-12-18 10:31:04 -0500

    Greatly limit what we're speccing. Right now it's just making sure the crypto provider works as expected.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,6 @@
-# require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
 require 'rubygems'
-require 'active_support'
-require 'active_record'
 require 'spec'
-
 require 'invision_bridge'
-require File.expand_path(File.dirname(__FILE__) + "/blueprints")
-
-TEST_DATABASE_FILE = File.join(File.dirname(__FILE__), '..', 'test.sqlite3')
-
-File.unlink(TEST_DATABASE_FILE) if File.exist?(TEST_DATABASE_FILE)
-ActiveRecord::Base.establish_connection(
-  "adapter" => "sqlite3", "database" => TEST_DATABASE_FILE
-)
-
-RAILS_DEFAULT_LOGGER = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
-
-load(File.dirname(__FILE__) + '/schema.rb')
 
 $: << File.join(File.dirname(__FILE__), '..', 'lib')
 require File.join(File.dirname(__FILE__), '..', 'init')
--- tasks/invision_bridge_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :invision_bridge do
-#   # Task goes here
-# end
