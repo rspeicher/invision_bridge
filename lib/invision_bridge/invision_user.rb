9f54c63c27e8ac48ca0fb1dc3f1dfcf68513c704
rspeicher <rspeicher@gmail.com>
2009-12-18 10:30:36 -0500

    Add Juggernaut-specific stuff back to InvisionUser for now, to save me some headache while I learn.

--- lib/invision_bridge/invision_user.rb
+++ lib/invision_bridge/invision_user.rb
@@ -9,25 +9,31 @@ module InvisionBridge
     acts_as_authentic do |c|
       c.crypto_provider          = Authlogic::CryptoProviders::InvisionPowerBoard
       c.login_field              = :name
-      c.crypted_password_field   = :converge_password
-      c.password_salt_field      = :converge_salt
+      c.crypted_password_field   = :members_pass_hash
+      c.password_salt_field      = :members_pass_salt
       # c.validate_fields          = false
       c.validate_password_field  = false
     end
-  
-    def converge_password
-      self.converge.converge_pass_hash
-    end
-    def converge_salt
-      self.converge.converge_pass_salt
-    end
-  
+
+    # ---------------------------------------------------------------------------
     # Override some AR methods; we don't want to mess with Invision's integrity
     def destroy; end
     def delete; end
     def self.destroy_all; end
     def self.delete_all; end
-  
-    has_one :converge, :class_name => "InvisionUserConverge", :foreign_key => "converge_id"
+    
+    # Juggernaut-only stuff ----------------------------------------------------
+    # TODO: Remove this stuff and only do it in Juggernaut-related apps
+    
+    ADMIN_GROUP     = 4
+    MEMBER_GROUP    = 8
+    APPLICANT_GROUP = 9
+    
+    named_scope :juggernaut, :order => 'name', :conditions => { :mgroup => [ADMIN_GROUP, MEMBER_GROUP, APPLICANT_GROUP] }
+    has_one :member, :foreign_key => "user_id"
+    
+    def is_admin?
+      self.mgroup == ADMIN_GROUP
+    end
   end
 end
